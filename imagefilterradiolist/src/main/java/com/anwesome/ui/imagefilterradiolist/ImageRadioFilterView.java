cc7076abc29400a5bad875bd08c4c9b765609c4b
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 08:26:54 +0530

    adding draw and handletap methods for Radio

--- imagefilterradiolist/src/main/java/com/anwesome/ui/imagefilterradiolist/ImageRadioFilterView.java
+++ imagefilterradiolist/src/main/java/com/anwesome/ui/imagefilterradiolist/ImageRadioFilterView.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.imagefilterradiolist;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -29,11 +30,27 @@ public class ImageRadioFilterView extends View{
         return true;
     }
     private class Radio {
+        private float deg = 0;
         private int x,y,r;
         public Radio() {
             x = w/2;
             y = h-h/10;
             r = h/10;
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(x,y);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(0,0,r,paint);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawArc(new RectF(-r,-r,r,r),0,deg,true,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            deg = 360*factor;
+        }
+        public boolean handleTap(float x,float y) {
+            return x >= (this.x-this.r) && x <= (this.x+this.r) && y >= (this.y-this.r) && y <= (this.y+this.r);
         }
     }
 }
