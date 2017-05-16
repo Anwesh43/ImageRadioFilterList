1807cabdb811432ac1d2900188ba47219dad6d7f
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 08:21:16 +0530

    Initializing dimensions

--- imagefilterradiolist/src/main/java/com/anwesome/ui/imagefilterradiolist/ImageRadioFilterView.java
+++ imagefilterradiolist/src/main/java/com/anwesome/ui/imagefilterradiolist/ImageRadioFilterView.java
@@ -11,16 +11,29 @@ import android.view.View;
  */
 public class ImageRadioFilterView extends View{
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int w,h,time = 0;
     public ImageRadioFilterView(Context context) {
         super(context);
     }
     public void onDraw(Canvas canvas) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN){
 
         }
         return true;
+    }
+    private class Radio {
+        private int x,y,r;
+        public Radio() {
+            x = w/2;
+            y = h-h/10;
+            r = h/10;
+        }
     }
 }
