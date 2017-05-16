fec5885ec2c1593e4a6c0f8a5ecc2e00b1f7b4c1
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 09:17:51 +0530

    Using OnSelectionChangeListener

--- app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
@@ -1,21 +1,39 @@
 package com.anwesome.ui.imagefilterradiolistdemo;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.anwesome.ui.imagefilterradiolist.ImageRadioFilterView;
+import com.anwesome.ui.imagefilterradiolist.OnSelectionChangeListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.forest1);
-        ImageRadioFilterView imageRadioFilterView = new ImageRadioFilterView(this,bitmap);
-        addContentView(imageRadioFilterView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,300));
+        if(bitmap == null) {
+            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.forest1);
+        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ImageRadioFilterView imageRadioFilterView = new ImageRadioFilterView(this, bitmap);
+        addContentView(imageRadioFilterView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,500));
+        imageRadioFilterView.setOnSelectionChangeListener(new OnSelectionChangeListener() {
+            @Override
+            public void onSelect() {
+                Toast.makeText(MainActivity.this, "Selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUnSelect() {
+                Toast.makeText(MainActivity.this, "UnSelected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
