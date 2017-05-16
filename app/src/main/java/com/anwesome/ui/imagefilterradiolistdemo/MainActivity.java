bdd952ed0c0fe4002d849a89b482fc7a2d2bc4e8
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 09:04:33 +0530

    Using ImageRadioFilterView in app

--- app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.anwesome.ui.imagefilterradiolistdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.imagefilterradiolist.ImageRadioFilterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.forest1);
+        ImageRadioFilterView imageRadioFilterView = new ImageRadioFilterView(this,bitmap);
+        addContentView(imageRadioFilterView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,300));
     }
 }
