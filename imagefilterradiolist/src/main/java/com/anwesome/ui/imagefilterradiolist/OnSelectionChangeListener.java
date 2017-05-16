b0ff9cc85b370212ee4566da40380aff372f5076
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 08:50:44 +0530

    adding onselectionchangelistener

---
+++ imagefilterradiolist/src/main/java/com/anwesome/ui/imagefilterradiolist/OnSelectionChangeListener.java
@@ -0,0 +1,9 @@
+package com.anwesome.ui.imagefilterradiolist;
+
+/**
+ * Created by anweshmishra on 16/05/17.
+ */
+public interface OnSelectionChangeListener {
+    void onSelect();
+    void onUnSelect();
+}
