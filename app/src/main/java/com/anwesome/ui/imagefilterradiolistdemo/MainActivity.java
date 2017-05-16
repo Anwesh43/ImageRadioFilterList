1bc51ddf2332cd7eafd8536dbdc398e6e7cd6115
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 09:53:58 +0530

    Using ImageFilterVideoList in app

--- app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/imagefilterradiolistdemo/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.anwesome.ui.imagefilterradiolist.ImageFilterRadioList;
 import com.anwesome.ui.imagefilterradiolist.ImageRadioFilterView;
 import com.anwesome.ui.imagefilterradiolist.OnSelectionChangeListener;
 
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         if(bitmap == null) {
             if(savedInstanceState!=null && savedInstanceState.getParcelable(bitmapKey) != null) {
                 bitmap = savedInstanceState.getParcelable(bitmapKey);
@@ -27,20 +27,22 @@ public class MainActivity extends AppCompatActivity {
                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.forest1);
             }
         }
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        ImageRadioFilterView imageRadioFilterView = new ImageRadioFilterView(this, bitmap);
-        addContentView(imageRadioFilterView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,500));
-        imageRadioFilterView.setOnSelectionChangeListener(new OnSelectionChangeListener() {
-            @Override
-            public void onSelect() {
-                Toast.makeText(MainActivity.this, "Selected", Toast.LENGTH_SHORT).show();
-            }
+        ImageFilterRadioList imageFilterRadioList = new ImageFilterRadioList(this);
+        for(int i=0;i<15;i++) {
+            final int index = i;
+            imageFilterRadioList.addImage(bitmap,new OnSelectionChangeListener() {
+                @Override
+                public void onSelect() {
+                    Toast.makeText(MainActivity.this, (index+1)+"Selected", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onUnSelect() {
-                Toast.makeText(MainActivity.this, "UnSelected", Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void onUnSelect() {
+                    Toast.makeText(MainActivity.this, (index+1)+"UnSelected", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        imageFilterRadioList.show();
     }
     public void onSaveInstanceState(Bundle bundle) {
         if(bitmap != null) {
